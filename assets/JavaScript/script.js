28baaeccf07e4ed0b529f35c7a092d77a96875fa
MichaelRempe <michaeljrempe@gmail.com>
2020-01-13 14:03:50 -0500

    Adaptation of global vals for data manipulation

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,25 +1,82 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // INPUT FIELD DOM
+    var inputField = $("#queryInput");
+    var inputBTN = $("#queryBTN");
+    // JUMBOTRON VALUES and DISPLAYS
+    var tempDSP = $("#currentTemp");
+    var humidityDSP = $("#currentHumidity");
+    var wind_speedDSP = $("#currentWindSpeed"); 
+    var uv_indexDSP = $("#currentUV");
+        //default jumbotron values
+    var temp = "-";
+    var humidity = "-";
+    var wind_speed = "-";
+    var uv_index = "-";
+
+    // 5-DAY CARDs
+
+    // API global vals
     var key = "b452ea12ae8781f3b7e10bd1f1ed3249";
-    var query = "" //captured input;
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=Washington%20DC&APPID=b452ea12ae8781f3b7e10bd1f1ed3249"; //"...?q="+query
+    var query = ""; //captured input;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather"; //"...?q="+query
     
-    function setPopCities(){
-        var popCities = $(".list-group-item");
-        console.log(popCities);
-        console.log(popCities[0].val());
-        // for(var i = 0; popCities.length; i++){
-        //     // $("<li>") = popCities[i];
-            
-        //     // popCities[i].addEventListener("click", function(){
-        //     //     console.log($(this).attr("value"));
-        //     // });
-        // }
-    }
-    setPopCities();
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+
+    
+
+    // Search bar input field and button
+    inputBTN.on("click", function(){
+        captureInput();
     });
+    
+    //capture input from text field
+    function captureInput(){
+        query = inputField.val();
+        requestAPI();
+    }
+
+    //main function handling page functionality
+    function main(){
+        renderPopCities();
+        renderJumbo();
+        renderCards();
+        
+    }
+    //Dynamic creation of group items of popular cities
+    //obtains list group and appends jquery list items with bootstrap attributes. Based on popCities array
+    // predefined pop city arry
+    var popCities = ["Boston", "Washington DC", "New York", "Tampa", "New Oreleans", "Oklahoma City", "Las Vegas", "Seatle"];
+    function renderPopCities() {
+        var popCitiesLG = $(".list-group");
+        for (var i = 0; i < popCities.length; i++) {
+            //creat list item
+            var city = $("<li>");
+            // add attributes and event listener
+            city.text(popCities[i]);
+            city.attr("value", popCities[i]);
+            city.attr("class", "list-group-item");
+            city.attr("type", "button");
+            city.removeClass("active");
+            // pass user click to query to re-render card states
+            city.on("click", function () {
+                $(this).attr("class", "list-group-item active");
+                setPopCities();
+                query = $(this).attr("value");
+                requestAPI();
+            });
+            popCitiesLG.append(city);
+        }
+    }
+    function requestAPI() {
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            data: {
+                q: query,
+                APPID : key
+            }
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
+    main();
 });
