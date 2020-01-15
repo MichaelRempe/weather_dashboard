9edaaa417695cb5a2c6e7711981218b191f1e1ef
MichaelRempe <michaeljrempe@gmail.com>
2020-01-14 19:53:17 -0500

    Updated forecast api handling

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -9,27 +9,26 @@ $(document).ready(function () {
     var humidityDSP = $("#currentHumidity");
     var wind_speedDSP = $("#currentWindSpeed"); 
     var uv_indexDSP = $("#currentUV");
+    var iconDSP = $("#current_icon");
         //default jumbotron values
     var temp = "-";
     var humidity = "-";
     var wind_speed = "-";
     var uv_index = "-";
-    // var latitude = "";
-    // var longitude = "";
     var current_city="Search for a city!";
     var current_time="";
+    var main_icon="";
 
     // 5-DAY CARDs
-    var cardDSP = $(".card-display");
+    var cards = [$(".card-0"), $(".card-1"), $(".card-2"), $(".card-3"), $(".card-4")];
 
     // API global vals
     var key = "b452ea12ae8781f3b7e10bd1f1ed3249";
     var query = ""; //captured input;
     var queryURL = "http://api.openweathermap.org/data/2.5/weather"; //"...?q="+query
     
-
-    
-
+    // Global Count of items in local storage
+    var count = 0;
     // Search bar input field and button
     inputBTN.on("click", function(){
         captureInput();
@@ -50,7 +49,7 @@ $(document).ready(function () {
     //Dynamic creation of group items of popular cities
     //obtains list group and appends jquery list items with bootstrap attributes. Based on popCities array
     // predefined pop city arry
-    var popCities = ["Boston", "Washington DC", "New York", "Tampa", "Huston", "Kansas City", "Las Vegas", "Seattle"];
+    var popCities = ["Boston", "Washington", "New York City", "Tampa", "Huston", "Kansas City", "Las Vegas", "Seattle"];
     function renderPopCities() {
         var popCitiesLG = $(".list-group");
         for (var i = 0; i < popCities.length; i++) {
@@ -76,6 +75,31 @@ $(document).ready(function () {
             });
             popCitiesLG.append(city);
         }
+        //add onto list based on local storage using same for loop logic for clearing pop cities div and re rendering list
+        for(var i=0; i<count; i++){
+            if(popCities.includes(localStorage.getItem(i)) === false){
+                 //create list item
+             var city = $("<li>");
+             // add attributes and event listener
+             city.text(localStorage.getItem(i));
+             city.attr("value", localStorage.getItem(i));
+             city.attr("class", "list-group-item");
+             city.attr("type", "button");
+             city.removeClass("active");
+             city.on("click", function () {
+                query = $(this).attr("value");  //set query to selected city
+                popCitiesLG.empty(); //clear list and re-render with new active attribue set
+                renderPopCities();
+                var active = $(this).attr("value");
+                $("li[value|='"+active+"']").attr("class","list-group-item active");
+                $("li[value|='"+active+"']").attr("style", "background-color: orange");
+
+                $(this).attr("class", "list-group-item active");
+                requestAPI();
+            });
+            popCitiesLG.append(city);
+         }
+        }
     }
     //Render Jumbo displays results of response data
     function aquireJumbo(data){
@@ -85,14 +109,13 @@ $(document).ready(function () {
         temp = Math.round((data.main.temp -273.15)*9/5+32)+"°F."; //ferenheight conversion from Kelvin
         humidity = data.main.humidity+" %";
         wind_speed = data.wind.speed+" MPH";
+        main_icon = data.weather.icon;
         var latitude = data.coord.lat;
         var longitude = data.coord.lon;
-        // console.log(latitude);
-        // console.log(longitude);
 
         // seperate request for UV index based on initial city lat and long
         $.ajax({
-            url: "http://samples.openweathermap.org/data/2.5/uvi?",
+            url: "http://api.openweathermap.org/data/2.5/uvi?",
             method: "GET",
             data: {
                 APPID : key,
@@ -100,7 +123,6 @@ $(document).ready(function () {
                 lon: longitude,
             }
         }).then(function (response) {
-            // console.log(response);
             uv_index = response.value;
             // Now that we have all data we can render to DOM
             renderJumbo();
@@ -110,6 +132,7 @@ $(document).ready(function () {
         //Set Span text of each cat to data
         cityDSP.text(current_city);
         timeDSP.text(current_time);
+        iconDSP.attr("src", main_icon);
         tempDSP.text(temp);
         humidityDSP.text(humidity);
         wind_speedDSP.text(wind_speed);
@@ -120,36 +143,41 @@ $(document).ready(function () {
 // renders blnak bootsrap cards (forcast_data will eventually be incorporated)
     function aquireCardData(forcast_data){
         console.log(forcast_data);
-        for(var i=0; i<5; i++){
-            var col = $("<div>");
-            col.attr("class", "col-2");
-            var card = $("<div>");
-            card.attr("class", "card");
-            var header = $("<div>");
-            header.attr("class", "card-header");
-            var icon = $("<img>");
-            icon.attr("src", "...");
-            var body = $("<div>");
-            body.attr("class", "card-body");
-            var card_temp = $("<p>");
-            card_temp.attr("class", "card-text");
-            var card_humid = $("<p>");
-            card_humid.attr("class", "card-text");
-
-            body.append(card_temp);
-            body.append(card_humid);
-
-            card.append(header);
-            card.append(icon);
-            card.append(body);
-
-            col.append(card);
-            cardDSP.append(col);
+        var card_count = 0;
+        console.log(forcast_data.list[count].main.temp);
+        for(var i=0; i<cards.length; i++){
+            for(card_count; count<forcast_data.length; count+=8){
+                var card = cards[i];
+                card.empty();
+                var header = $("<div>");
+                header.attr("class", "card-header");
+                header.text(forcast_data.list[count].dt_txt);
+                var icon = $("<img>");
+                icon.attr("src", forcast_data.list[count].weather[0].icon);
+                var body = $("<div>");
+                body.attr("class", "card-body");
+                var card_temp = $("<p>");
+                card_temp.attr("class", "card-text");
+                card_temp.text(Math.round((forcast_data.list[count].main.temp -273.15)*9/5+32)+"°F.")
+                var card_humid = $("<p>");
+                card_humid.attr("class", "card-text");
+                card_humid.text(forcast_data.list[count].main.humidity);
+                //attach card data to card
+                body.append(card_temp);
+                body.append(card_humid);
+    
+                card.append(header);
+                card.append(icon);
+                card.append(body);
+                
+                console.log(cards[i]);
+                cards[i].append(card);
+            }
         }
+           
     }
     function requestAPI() {
         $.ajax({
-            
             url: queryURL,
             method: "GET",
             data: {
@@ -159,13 +187,14 @@ $(document).ready(function () {
         }).then(function (data) {
             aquireJumbo(data);
             requestForcastAPI();
-            
+            localStorage.setItem(count, data.name);
+            count++;
         });
     }
     function requestForcastAPI(){
         console.log(query);
         $.ajax({
-            url: "api.openweathermap.org/data/2.5/forecast?",
+            url: "http://api.openweathermap.org/data/2.5/forecast?",
             method: "GET",
             data:{
                 q: query,
